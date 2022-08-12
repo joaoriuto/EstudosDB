3f1dfa132571db8d92508601ad08e9b9053ce00f
joaoriuto <jr00.dat@gmail.com>
2022-08-11 18:06:10 -0700

    DDL - Estudos de Definição de dados

--- Exercicios/CLIENTE/INSERTS_TB_COMERCIO.sql
+++ Exercicios/CLIENTE/INSERTS_TB_COMERCIO.sql
@@ -230,3 +230,73 @@ INSERT INTO CLIENTE VALUES(NULL,'ERRADAO ID 12','M',NULL,'EREASEME');
 SELECT * FROM CLIENTE WHERE IDCLIENTE = 12;
 
 DELETE FROM CLIENTE WHERE IDCLIENTE = 12;
+
+--> DATA DEFINITION LANGUAGE
+
+CREATE TABLE PRODUTO(
+	IDPRODUTO INT PRIMARY KEY AUTO_INCREMENT,
+	NOME_PRODUTO VARCHAR(30) NOT NULL,
+	PRECO INT,
+	FRETE FLOAT(10,2) NOT NULL
+);
+
++--------------+-------------+------+-----+---------+----------------+
+| Field        | Type        | Null | Key | Default | Extra          |
++--------------+-------------+------+-----+---------+----------------+
+| IDPRODUTO    | int(11)     | NO   | PRI | NULL    | auto_increment |
+| NOME_PRODUTO | varchar(30) | NO   |     | NULL    |                |
+| PRECO        | int(11)     | YES  |     | NULL    |                |
+| FRETE        | float(10,2) | NO   |     | NULL    |                |
++--------------+-------------+------+-----+---------+----------------+
+
+--> ALTER TABLE
+--> ALTERANDO O NOME DE UMA COLUNA - CHANGE
+
+ALTER TABLE PRODUTO
+CHANGE PRECO VALOR_UNIT INT NOT NULL; --> PERMITE MODIFICAR NOME, TIPO E CLAUSULA
+
++--------------+-------------+------+-----+---------+----------------+
+| Field        | Type        | Null | Key | Default | Extra          |
++--------------+-------------+------+-----+---------+----------------+
+| IDPRODUTO    | int(11)     | NO   | PRI | NULL    | auto_increment |
+| NOME_PRODUTO | varchar(30) | NO   |     | NULL    |                |
+| VALOR_UNIT   | int(11)     | NO   |     | NULL    |                |
+| FRETE        | float(10,2) | NO   |     | NULL    |                |
++--------------+-------------+------+-----+---------+----------------+
+
+--> MODIFY - ALTERANDO O TIPO
+
+ALTER TABLE PRODUTO
+MODIFY VALOR_UNIT VARCHAR(50) NOT NULL;
+
+--> ADICIONANDO COLUNAS
+
+ALTER TABLE PRODUTO
+ADD PESO FLOAT(10,2) NOT NULL;
+
+--> APAGANDO PRODUTO
+
+ALTER TABLE PRODUTO
+DROP COLUMN PESO;
+
+--> ADICIONANDO UMA COLUNA EM ORDEM ESPECÍFICA
+
+ALTER TABLE PRODUTO
+ADD COLUMN PESO FLOAT(10,2) NOT NULL
+AFTER NOME_PRODUTO;
+
++--------------+-------------+------+-----+---------+----------------+
+| Field        | Type        | Null | Key | Default | Extra          |
++--------------+-------------+------+-----+---------+----------------+
+| IDPRODUTO    | int(11)     | NO   | PRI | NULL    | auto_increment |
+| NOME_PRODUTO | varchar(30) | NO   |     | NULL    |                |
+| PESO         | float(10,2) | NO   |     | NULL    |                |
+| VALOR_UNIT   | varchar(50) | NO   |     | NULL    |                |
+| FRETE        | float(10,2) | NO   |     | NULL    |                |
++--------------+-------------+------+-----+---------+----------------+
+
+--> ADICIONANDO EM PRIMEIRA POSIÇÃO
+
+ALTER TABLE PRODUTO
+ADD COLUMN PESO FLOAT(10,2) NOT NULL
+FIRST;
